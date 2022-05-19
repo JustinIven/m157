42a1881121d5f6708c1f33d3583ec094a50b996b
Justin Iven Müller <justinivenmuller@gmail.com>
2022-05-19 22:43:16 +0200

    help

--- install.ps1
+++ install.ps1
@@ -1,16 +1,46 @@
-[CmdletBinding()]
+<#
+.SYNOPSIS
+Script to install choco and packages on a remote host.
+
+.DESCRIPTION
+This script will install either choco or choco and packages on a remote host with a PowerShell session.
+
+.PARAMETER client
+One or more clients to install choco and packages on.
+
+.PARAMETER package
+One or more packages to install on the remote host(s).
+
+.PARAMETER username
+The username to use when connecting to the remote host(s).
+
+.PARAMETER password
+The password as a SecureString to use when connecting to the remote host(s). If the password is not specified, the user will be prompted to enter the password.
+
+.EXAMPLE
+PS> .\install.ps1 -clients 192.168.116.132 -packages git.install -username vmadmin
+
+.EXAMPLE
+PS> .\install.ps1 -clients host1, host2 -packages git.install, adobereader -username vmadmin -password ****
+#>
+
 param (
+    [Parameter(Mandatory = $true, ParameterSetName = 'installChoco')]
+    [Parameter(Mandatory = $true, ParameterSetName = 'installAll')]
     [string[]]
     $clients,
 
+    [Parameter(Mandatory = $true, ParameterSetName = 'installAll')]
     [string[]]
     $packages,
 
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $true, ParameterSetName = 'installChoco')]
+    [Parameter(Mandatory = $true, ParameterSetName = 'installAll')]
     [string]
     $username,
 
-    [Parameter(Mandatory = $true)]
+    [Parameter(Mandatory = $false, ParameterSetName = 'installChoco')]
+    [Parameter(Mandatory = $false, ParameterSetName = 'installAll')]
     [SecureString]
     $password
 )
@@ -19,6 +49,7 @@ foreach ($i in $clients) {
     $cred = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $username, $password
 
     $scriptBlock = {
+        param($i, $packages)
         Write-Host "verifying chocolatey is installed on host $i"
         if (!(Test-Path "$($env:ProgramData)\chocolatey\choco.exe")) {
             Write-FPLog -Category Info -Message "installing chocolatey..."
@@ -46,5 +77,5 @@ foreach ($i in $clients) {
         }
     }
 
-    Invoke-Command -Command $scriptBlock -computerName $i -credential $cred 
+    Invoke-Command -Command $scriptBlock -computerName $i -credential $cred -ArgumentList $i, $packages
 }
